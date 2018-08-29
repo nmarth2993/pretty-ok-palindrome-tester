d80acd2a532bb88b2f04639ab292cd5c8fec1279
nmarth <nmarth2993@gmail.com>
2018-08-29 14:10:18 -0400

    typo

--- palindrome.java
+++ palindrome.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class palindrome {
-	public static viod main(String[] args) {
+	public static void main(String[] args) {
 		String userInput, reverse;
 		Scanner i = new Scanner(System.in);
 		System.out.print("Enter a string to test if palindrome: ");
